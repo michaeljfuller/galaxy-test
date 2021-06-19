88b4f11760b5e3552b9094e9611e81734479710f
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-06-19 11:44:24 +0100

    Add sass & react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import css from './App.module.scss';
 
 function App() {
+
+  console.log('css', css);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={css.root}>
+      <header className={css.header}>
+        <img src={logo} className={css.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={css.link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"

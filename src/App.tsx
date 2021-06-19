e73e52d3ff2db2e2f97464e56c23388269f46d13
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-06-19 11:48:13 +0100

    Clear out template

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import css from './App.module.scss';
 
 function App() {
-
-  console.log('css', css);
-  return (
-    <div className={css.root}>
-      <header className={css.header}>
-        <img src={logo} className={css.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={css.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={css.root}>
+            Hello World
+        </div>
+    );
 }
 
 export default App;

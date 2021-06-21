7656b4c30d4525622198eefc25d605ba1c15a911
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-06-21 16:26:09 +0100

    Add content to ApiProfileSection
    Add disabled styles for input.
    Add margin under section subtitles

--- src/screens/ProfileScreen/sections/ApiProfileSection.tsx
+++ src/screens/ProfileScreen/sections/ApiProfileSection.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import css from "./ApiProfileSection.module.scss";
+import useAuth from "../../../hooks/useAuth";
 
 export interface ApiProfileSectionProps {}
 
 export function ApiProfileSection(props: ApiProfileSectionProps) {
+    const user = useAuth().currentUser as User;
+
     return <section className={css.root}>
         <h2 className={css.title}>Access token</h2>
         <p className={css.subtitle}>Generate your unique key to access Galactic Data™.</p>
+
+        <div className={css.row}>
+            <label className={css.key}>
+                Your unique key
+                <input readOnly disabled value={user.token} />
+            </label>
+        </div>
+
+        <button>Generate key</button>
+
     </section>;
 }
 export default ApiProfileSection;

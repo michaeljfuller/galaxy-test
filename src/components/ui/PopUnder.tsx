7c2001c33f67e95327f56599c57c5b1c280bf939
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-06-23 13:25:34 +0100

    Add PopUnder component

---
+++ src/components/ui/PopUnder.tsx
@@ -0,0 +1,21 @@
+import React, {ComponentType, CSSProperties, PropsWithChildren} from "react";
+import css from "./PopUnder/PopUnder.module.scss";
+import {classNames} from "../../utils/component-utils";
+
+export type PopUnderProps = PropsWithChildren<{
+    open: boolean;
+    content: ComponentType;
+    className?: string;
+    style?: CSSProperties;
+    containerClassName?: string;
+    containerStyle?: CSSProperties;
+}>;
+
+export default function PopUnder(props: PopUnderProps) {
+    return <div className={classNames(css.root, props.containerClassName)} style={props.containerStyle}>
+        {props.children}
+        {props.open ? <div className={classNames(css.popUnder, props.className)} style={props.style}>
+            {React.createElement(props.content)}
+        </div> : null}
+    </div>;
+}
